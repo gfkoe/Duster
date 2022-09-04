33e0f55fb13843db7009f2949ea5bfc890d392f3
Gabriel Koeb <gfkoeb@ncsu.edu>
2022-09-03 22:47:24 -0400

    Added bot to server and basic command ability

--- Duster/src/index.js
+++ Duster/src/index.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
-const Discord = { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits } = require('discord.js');
+const TOKEN = process.env.TOKEN;
+
 
 const client = new Discord.Client({
   intents: [
@@ -8,9 +10,19 @@ const client = new Discord.Client({
   ]
 })
 
-const TOKEN = process.env.TOKEN;
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+
 client.login(TOKEN);
 client.on('ready', () => {
     console.log('Logged in as Duster!');
 })
 
+client.on('message', msg => {
+  if (msg.content.toLowerCase.includes('!test')) {
+    msg.reply('https://www.dustloop.com/w/GGST/Testament');
+    msg.channel.send('https://www.dustloop.com/w/GGST/Testament');
+  }
+});
